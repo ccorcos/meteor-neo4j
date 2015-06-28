2b953b4d4281c8cfd063523a48bb73ac9d57a6d0
Chet Corcos <ccorcos@gmail.com>
2015-06-28 10:22:57 -0700

    new version

---
+++ HISTORY.md
@@ -0,0 +1,5 @@
+# 0.0.3
+
+- added m4j CLI utility
+- added a better reset method
+- added `isEmpty`
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ccorcos:neo4j',
   summary: 'Neo4j API for Meteor',
-  version: '0.0.2',
+  version: '0.0.3',
   git: 'https://github.com/ccorcos/meteor-neo4j'
 });
 
